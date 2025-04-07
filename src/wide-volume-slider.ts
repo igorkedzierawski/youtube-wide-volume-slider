91e155b2dbbb8a64b5a9f54ad3f31d312565491d
Igor Kedzierawski <igorkedzierawski@mail.com>
2025-04-07 23:28:49 +0200

    improved wide slider style

--- src/wide-volume-slider.ts
+++ src/wide-volume-slider.ts
@@ -19,11 +19,57 @@ const createWideVolumeSliderElement = (): HTMLElement => {
             align-items: center;
             justify-content: center;
             margin: 8px;
+            --track-width: 180px;
+            --thumb-size: 16px;
+            --track-thickness: 5px;
+            --hitbox-padding: 5px;
         }
-        #youtube-wide-volume-slider-container input {
-            width: 180px;
+
+        #youtube-wide-volume-slider-container input[type="range"] {
+            width: var(--track-width);
             margin-right: 4px;
+            -webkit-appearance: none;
+            appearance: none;
+            background: transparent;
+
+            /* increase clickable area */
+            padding: var(--hitbox-padding) 0;
+            margin: calc(-1 * var(--hitbox-padding)) 0;
+            height: calc(var(--track-thickness) + 2 * var(--hitbox-padding));
+            box-sizing: content-box;
         }
+
+        /* Track */
+        #youtube-wide-volume-slider-container input[type="range"]::-webkit-slider-runnable-track {
+            background: white !important;
+            height: var(--track-thickness);
+            border-radius: calc(var(--track-thickness) / 2);
+        }
+
+        #youtube-wide-volume-slider-container input[type="range"]::-moz-range-track {
+            background: white !important;
+            height: var(--track-thickness);
+            border-radius: calc(var(--track-thickness) / 2);
+        }
+
+        /* Thumb */
+        #youtube-wide-volume-slider-container input[type="range"]::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            height: var(--thumb-size);
+            width: var(--thumb-size);
+            background: white !important;
+            border-radius: 50%;
+            margin-top: calc((var(--track-thickness) - var(--thumb-size)) / 2);
+        }
+
+        #youtube-wide-volume-slider-container input[type="range"]::-moz-range-thumb {
+            height: var(--thumb-size);
+            width: var(--thumb-size);
+            background: white !important;
+            border-radius: 50%;
+            border: none;
+        }
+
         #youtube-wide-volume-slider-container button {
             color: white;
             background-color: #0000;
