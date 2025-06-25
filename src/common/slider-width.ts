5c194462a734f9a35843fb74764c202fbf3eeaa9
Igor Kedzierawski <igorkedzierawski@mail.com>
2025-06-25 21:04:32 +0000

    created popup for slider's width selection and enabling its responsivenes; hooked up values selected via popup to the slider component; refactored volume slider component creation and some utility functions;

---
+++ src/common/slider-width.ts
@@ -0,0 +1,7 @@
+export const YT_DEFAULT_SLIDER_WIDTH_PX = 48;
+
+export const AVAILABLE_WIDTH_MULTIPLIERS = [
+    1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.5, 5, 5.5, 6,
+];
+export const DEFAULT_WIDTH_MULTIPLIER_INDEX = 9;
+
